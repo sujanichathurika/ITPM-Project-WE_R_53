f8f08079ad4b6194b1a50b7be23ba908e3a82df6
sujanichathurika <chathusujani57@gmail.com>
2021-05-20 17:14:13 +0530

    After adding all the Funcatios

---
+++ ITPM_IT19040318/src/Member_1/View_Parallel.java
@@ -0,0 +1,120 @@
+package Member_1;
+
+import java.awt.Color;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
+public class View_Parallel {
+
+	 JFrame frame;
+	 private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View_Parallel window = new View_Parallel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public View_Parallel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBackground(Color.WHITE);
+		frame.setAlwaysOnTop(true);
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel company_name = new JLabel("ABC INSTITUTE TIME TABLE MANAGEMNT SYSTEM");
+		company_name.setBackground(Color.LIGHT_GRAY);
+		company_name.setForeground(new Color(0, 0, 128));
+		company_name.setHorizontalAlignment(SwingConstants.CENTER);
+		company_name.setFont(new Font("Sylfaen", Font.BOLD, 28));
+		company_name.setBounds(184, 0, 836, 93);
+		frame.getContentPane().add(company_name);
+		
+		JButton company_icon = new JButton("");
+		//add company icon
+		Image img = new ImageIcon(this.getClass().getResource("/ABC_com_icon.png")).getImage();
+		company_icon.setIcon(new ImageIcon(img));
+		company_icon.setBackground(Color.LIGHT_GRAY);
+		company_icon.setForeground(Color.LIGHT_GRAY);
+		company_icon.setFont(new Font("Yu Gothic UI Light", Font.BOLD | Font.ITALIC, 43));
+		company_icon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//direct to  Add session Frame
+				
+				//create Object (interface name,object name,interface name ) and set the initial frame name and remove the public 
+				Add_Sessions Add_Sessions = new Add_Sessions();
+				Add_Sessions.frame.setVisible(true);
+				frame.dispose();
+				
+			}
+		});
+		company_icon.setBounds(95, 13, 77, 64);
+		frame.getContentPane().add(company_icon);
+		
+		JSeparator separator_main = new JSeparator();
+		separator_main.setBackground(Color.BLACK);
+		separator_main.setForeground(Color.BLACK);
+		separator_main.setBounds(0, 90, 1073, 3);
+		frame.getContentPane().add(separator_main);
+		
+		JLabel parallel = new JLabel("Parallel  Sessions");
+		parallel.setHorizontalAlignment(SwingConstants.CENTER);
+		parallel.setFont(new Font("Sylfaen", Font.BOLD, 20));
+		parallel.setBounds(260, 106, 639, 40);
+		frame.getContentPane().add(parallel);
+		
+		JButton btnNewButton = new JButton("Remove");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.setBackground(Color.RED);
+		btnNewButton.setBounds(748, 536, 188, 48);
+		frame.getContentPane().add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(164, 199, 755, 250);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setBackground(new Color(211, 211, 211));
+		frame.setBounds(200, 200, 1091, 739);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+}
+
---
+++ ITPM_IT19040318/src/Member_4/Main4.java
@@ -0,0 +1,151 @@
+package Member_4;
+/*package Member4;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Main4 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main4 window = new Main4();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the application.
+	 */
+	/*public Main4() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	/*private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(350, 90, 1350, 900);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);*/
+		
+		/*JButton btnLocation = new JButton("Add Location");
+		btnLocation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Location al = new Location();
+				try {
+					Location.main(null);
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.setVisible(false);
+			}
+		});
+		btnLocation.setBounds(95, 31, 269, 33);
+		frame.getContentPane().add(btnLocation);*/
+		
+		/*JButton btnManageLocation = new JButton("Manage Location");
+		btnManageLocation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Manage_Location ml = new Manage_Location();
+				try {
+					Manage_Location.main(null);
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.setVisible(false);	
+			}
+		});*/
+		/*btnManageLocation.setBounds(95, 113, 269, 33);
+		frame.getContentPane().add(btnManageLocation);*/
+		
+		/*JButton btnAddSessionRooms = new JButton("Add Session Rooms");
+		btnAddSessionRooms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddSessionRooms ar = new AddSessionRooms();
+				try {
+					AddSessionRooms.main(null);
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.setVisible(false);	
+			}
+		});
+		btnAddSessionRooms.setBounds(95, 210, 269, 33);
+		frame.getContentPane().add(btnAddSessionRooms);
+		
+		JButton btnManageSessionRooms = new JButton("Manage Session Rooms");
+		btnManageSessionRooms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ViewRooms msr = new ViewRooms();
+				try {
+					ViewRooms.main(null);
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InstantiationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IllegalAccessException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.setVisible(false);
+			}
+		});
+		btnManageSessionRooms.setBounds(105, 309, 259, 33);
+		frame.getContentPane().add(btnManageSessionRooms);
+	}
+}*/
